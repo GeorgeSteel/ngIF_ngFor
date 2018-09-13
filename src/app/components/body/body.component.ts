c16b9119f6c044f35cc2a651696b581cf3e6c689
Jorge Acero <julianacero98@gmail.com>
2018-09-13 08:39:38 -0500

    Inicio

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html'
+})
+export class BodyComponent {
+
+  mostrar = true;
+
+  frase:any = {
+    mensaje: 'Yo solo se, que no se nada',
+    autor: 'Socrates'
+  };
+
+  personajes:string[] = ['Spider-Man', 'Venom', 'Green Goblin'];
+}
